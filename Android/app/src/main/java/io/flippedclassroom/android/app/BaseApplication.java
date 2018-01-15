6cdbbc18c308edcb875ab6d30e936da9a5b21426
Totoro <1131225133@qq.com>
2018-01-15 20:01:57 +0800

    Create base architecture

--- Android/app/src/main/java/io/flippedclassroom/android/app/BaseApplication.java
+++ Android/app/src/main/java/io/flippedclassroom/android/app/BaseApplication.java
@@ -1,11 +1,14 @@
 package io.flippedclassroom.android.app;
 
 import android.app.Application;
+import android.preference.Preference;
 
-public class BaseApplication extends Application{
+import io.flippedclassroom.android.util.PreferenceUtils;
+
+public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        PreferenceUtils.init(this);
     }
 }
