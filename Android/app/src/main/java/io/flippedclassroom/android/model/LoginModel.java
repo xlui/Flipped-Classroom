42d1089451a66990539a5c38c98a6dfdd1e5e299
Totoro <1131225133@qq.com>
2018-01-22 11:09:23 +0800

    login and token code

--- Android/app/src/main/java/io/flippedclassroom/android/model/LoginModel.java
+++ Android/app/src/main/java/io/flippedclassroom/android/model/LoginModel.java
@@ -4,6 +4,4 @@ public class LoginModel {
     //TODO: 接受一个token作为参数，保存token到本地。有现成的我写好的Preference类可以调用。
 
     //TODO: 保存用户id到AppCache类。作为全局变量。
-
-    // TODO: 创建一个String变量作为model的类成员变量，用于保存用户id。再写一个方法接受传入的Id作为参数，赋值给类成员Id变量
 }
