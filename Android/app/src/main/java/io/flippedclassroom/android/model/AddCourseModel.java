0059ae6d716a9bff9a54b75f31b257eec7d1ff16
Totoro <1131225133@qq.com>
2018-02-04 17:25:10 +0800

    modify some codes

---
+++ Android/app/src/main/java/io/flippedclassroom/android/model/AddCourseModel.java
@@ -0,0 +1,8 @@
+package io.flippedclassroom.android.model;
+
+/**
+ * Created by Xkf on 2018/2/4.
+ */
+
+public class AddCourseModel {
+}
---
+++ Android/app/src/main/java/io/flippedclassroom/android/presenter/AddCoursePresenter.java
@@ -0,0 +1,8 @@
+package io.flippedclassroom.android.presenter;
+
+/**
+ * Created by Xkf on 2018/2/4.
+ */
+
+class AddCoursePresenter {
+}
